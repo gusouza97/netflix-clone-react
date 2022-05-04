e3b2231a44c5986bd8d086e83881b5c0ecfb445a
Gustavo Souza <36318691+gusouza97@users.noreply.github.com>
2022-05-03 23:41:40 -0300

    Config inicial webpack, babel e react

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  // Ajuda na debugacao do codigo em modo de desenvolvimento
+  devtool: "source-map",
+
+  entry: path.resolve(__dirname, "./src/index.js"), // string | object | array // defaults to ./src // Here the application starts executing // and webpack starts bundling
+
+  output: {
+    // options related to how webpack emits results
+
+    path: path.resolve(__dirname, "dist"), // string (default) // the target directory for all output files // must be an absolute path (use the Node.js path module)
+
+    filename: "bundle.js", // string (default) // the filename template for entry chunks
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  performance: {
+    hints: false,
+
+    maxEntrypointSize: 512000,
+
+    maxAssetSize: 512000,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+
+      filename: "./index.html",
+    }),
+  ],
+};
